77e9acd18bcaebc4dcfafb83adf19ae9249121cc
701-Coder <51012111+701-coder@users.noreply.github.com>
2021-11-26 13:06:05 +0800

    Add files via upload

---
+++ game.js
@@ -0,0 +1,167 @@
+var pydata=[['B09401064', '鄭宇彤     ', '派大星', 100, 100, 100, 100, 100, 100], ['B09401137', '葉瞻語     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09502156', '楊景淳     ', 'Supermangod', 100, 100, 40, 80, 100, 0], ['B10902001', '黃俊翔     ', 'CPass', 100, 100, 100, 100, 100, 100], ['B10902003', '吳孟哲     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902005', '陳竹欣     ', '芒果', 100, 100, 100, 100, 100, 100], ['B10902007', '游立宇     ', '芒果', 100, 10, 0, 100, 100, 0], ['B10902009', '劉恒毅     ', 'NightCoders', 100, 100, 100, 100, 100, 60], ['B10902011', '賴詠晴     ', 'NightCoders', 100, 100, 100, 100, 100, 30], ['B10902013', '鄒瀠瑢     ', 'NightCoders', 100, 60, 100, 100, 100, 60], ['B10902015', '蔡清育     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B10902017', '楊慶彥     ', '芒果', 100, 100, 100, 100, 90, 0], ['B10902019', '汪彥廷     ', 'Anonymous', 100, 100, 100, 100, 60, 80], ['B10902021', '張佑亘     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902023', '賴晨皓     ', 'Anonymous', 100, 100, 100, 100, 100, 20], ['B10902025', '陳楷元     ', 'NightCoders', 100, 100, 100, 90, 0, 20], ['B10902027', '曹哲維     ', 'CPass', 100, 70, 0, 100, 100, 60], ['B10902031', '馮柏翰     ', '派大星', 100, 100, 0, 90, 0, 0], ['B10902033', '林祐辰     ', '派大星', 100, 60, 20, 100, 20, 50], ['B10902035', '余泓寬     ', 'Anonymous', 0, 0, 0, 90, 0, 0], ['B10902037', '羅煜翔     ', '芒果', 100, 100, 0, 100, 60, 60], ['B10902039', '黃邦維     ', 'NightCoders', 0, 90, 40, 100, 100, 100], ['B10902041', '連奕維     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902043', '張宸熏     ', 'FuwaFuwaTime', 100, 100, 100, 100, 100, 100], ['B10902045', '吳沛昀     ', 'FuwaFuwaTime', 100, 100, 100, 100, 0, 60], ['B10902047', '陳暉桓     ', 'FuwaFuwaTime', 100, 100, 100, 100, 100, 60], ['B10902049', '王政越     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B10902051', '洪譽侖     ', 'Anonymous', 100, 50, 100, 100, 100, 100], ['B10902053', '丘文磊     ', '芒果', 0, 40, 0, 100, 20, 60], ['B10902055', '林佑丞     ', '芒果', 100, 100, 10, 100, 0, 0], ['B10902057', '陳翊嘉     ', '芒果', 100, 100, 80, 100, 80, 10], ['B10902059', '陳晉緯     ', '亡靈', 100, 100, 100, 100, 50, 20], ['B10902061', '葉祐誠     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902063', '江建陞     ', '芒果', 100, 40, 50, 100, 60, 100], ['B10902065', '黃品翰     ', 'Anonymous', 100, 80, 0, 100, 100, 60], ['B10902069', '楊智翰     ', 'Tentacode', 100, 100, 50, 100, 100, 100], ['B10902071', '周民恩     ', 'FuwaFuwaTime', 100, 100, 100, 100, 100, 60], ['B10902073', '陳儷其     ', 'Tentacode', 0, 0, 0, 100, 40, 0], ['B10902075', '王德意     ', '月光', 100, 100, 30, 100, 100, 100], ['B10902077', '黃保文     ', 'CPass', 10, 40, 0, 0, 0, 0], ['B10902085', '許博翔     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902087', '張馨尹     ', '亡靈', 0, 30, 0, 100, 0, 0], ['B10902089', '引田匡哉    ', 'Anonymous', 100, 30, 0, 10, 0, 0], ['B10902091', '荻本俊輝    ', '芒果', 100, 100, 100, 100, 100, 60], ['B10902095', '梁浩言     ', '芒果', 100, 30, 0, 100, 100, 0], ['B10902097', '程正熹     ', 'Anonymous', 0, 0, 0, 0, 0, 0], ['B10902099', '韓智杰     ', '派大星', 100, 100, 70, 100, 100, 100], ['B10902101', '鄭百里     ', 'CPass', 100, 100, 100, 100, 100, 100], ['B10902103', '毛翊蓁     ', '芒果', 0, 0, 0, 100, 0, 0], ['B10902107', '吳定融     ', '芒果', 100, 100, 100, 100, 100, 0], ['B10902109', '吳秉勳     ', '芒果', 100, 30, 0, 100, 0, 20], ['B10902111', '李季軒     ', 'Tentacode', 100, 90, 0, 100, 100, 100], ['B10902113', '李諺傑     ', '芒果', 100, 100, 100, 100, 100, 100], ['B10902115', '林文博     ', 'FuwaFuwaTime', 100, 70, 50, 100, 60, 0], ['B10902117', '范秉逸     ', 'Tentacode', 10, 0, 0, 100, 40, 0], ['B10902119', '張戎狄     ', 'NightCoders', 10, 0, 0, 100, 100, 60], ['B10902121', '許子胤     ', '派大星', 100, 100, 100, 100, 100, 100], ['B10902123', '許絜師     ', 'Anonymous', 10, 20, 0, 100, 0, 0], ['B10902125', '陳竹筠     ', '亡靈', 0, 0, 0, 100, 0, 0], ['B10902127', '陳若瑜     ', '芒果', 100, 100, 0, 100, 100, 0], ['B10902129', '游凱鈞     ', '派大星', 100, 100, 100, 100, 100, 100], ['B10902131', '楊翊廷     ', 'Anonymous', 100, 100, 0, 100, 70, 0], ['B10902133', '標彥廷     ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['T10902305', '林廷宇     ', 'Anonymous', 0, 0, 0, 0, 0, 0], ['B08508024', '陳威廷     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902025', '許庭碩     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902043', '沈竑文     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902045', '林昌泓     ', 'Anonymous', 100, 100, 0, 100, 0, 100], ['B09902057', '張揚婕     ', 'Anonymous', 10, 0, 0, 100, 10, 0], ['B09902060', '馮 楷     ', '計程車', 100, 100, 100, 100, 100, 100], ['B09902071', '林峻毅     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902091', '陳廷雍     ', 'Anonymous', 0, 0, 0, 0, 0, 0], ['B09902113', '周弘毅     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902125', '陳剛頡     ', '派大星', 100, 100, 100, 100, 100, 100], ['B10902105', '何珮瑄     ', '派大星', 0, 0, 0, 80, 0, 0], ['B08902067', '倪永珅     ', 'Anonymous', 0, 40, 0, 0, 0, 0], ['B07902118', '游立儒     ', 'Anonymous', 100, 100, 10, 100, 100, 100], ['R10922171', '陳嘉政     ', 'Anonymous', 100, 60, 0, 70, 0, 0], ['R10944041', '張琮渝     ', 'Anonymous', 100, 0, 40, 100, 100, 0], ['R08944063', '蔣奇桓     ', 'Anonymous', 0, 0, 0, 100, 100, 100], ['B09703062', '彭鈺斯     ', '芒果', 0, 100, 0, 100, 100, 100], ['B10902002', '楊昕叡     ', 'Tentacode', 100, 100, 100, 100, 100, 0], ['B10902004', '林浩丞     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B10902006', '詹子慶     ', 'CPass', 100, 100, 100, 100, 100, 100], ['B10902008', '劉亦翔     ', 'Anonymous', 100, 100, 100, 0, 10, 0], ['B10902010', '江翊宣     ', 'FuwaFuwaTime', 90, 100, 100, 100, 100, 100], ['B10902012', '張齊恩     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B10902014', '項  于    ', 'CPass', 100, 100, 0, 100, 100, 100], ['B10902020', '方聿丞     ', 'CPass', 0, 100, 0, 100, 100, 100], ['B10902022', '黃文宏     ', 'CPass', 60, 100, 100, 100, 100, 100], ['B10902024', '林宸宇     ', 'CPass', 100, 100, 100, 100, 100, 100], ['B10902026', '黎裕玄     ', 'Anonymous', 100, 100, 0, 90, 0, 0], ['B10902030', '蔡嘉哲     ', '亡靈', 100, 100, 100, 100, 10, 100], ['B10902032', '李沛宸     ', '派大星', 100, 100, 100, 100, 100, 100], ['B10902034', '廖奕鈞     ', '派大星', 0, 100, 100, 100, 100, 100], ['B10902036', '陳柏廷     ', 'Anonymous', 80, 100, 0, 100, 100, 100], ['B10902038', '陳元彬     ', 'NightCoders', 10, 100, 0, 100, 0, 0], ['B10902040', '林庭宇     ', '派大星', 0, 100, 0, 100, 0, 100], ['B10902042', '楊心岑     ', 'CPass', 40, 100, 100, 100, 100, 100], ['B10902044', '許少墉     ', '派大星', 80, 100, 0, 100, 100, 0], ['B10902046', '張睿桓     ', 'FuwaFuwaTime', 100, 100, 100, 100, 100, 100], ['B10902048', '林昀禾     ', 'Anonymous', 100, 100, 100, 0, 0, 100], ['B10902050', '梁仁瑋     ', '芒果', 100, 100, 100, 100, 100, 100], ['B10902052', '葉咸礽     ', 'Anonymous', 100, 100, 100, 100, 100, 20], ['B10902054', '沈威宏     ', '計程車', 10, 0, 0, 100, 0, 100], ['B10902056', '林家慶     ', 'CPass', 0, 100, 40, 70, 0, 50], ['B10902058', '胡桓碩     ', '派大星', 60, 100, 100, 100, 100, 100], ['B10902062', '林佳緯     ', '芒果', 100, 100, 100, 100, 100, 100], ['B10902064', '張晴昀     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902066', '羅濟恩     ', '月光', 0, 100, 0, 0, 0, 100], ['B10902068', '黃千睿     ', '芒果', 100, 100, 100, 20, 0, 100], ['B10902070', '林鈺翔     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902072', '林宏瑜     ', 'Anonymous', 0, 100, 100, 100, 100, 100], ['B10902074', '李佳謙     ', '亡靈', 0, 100, 30, 100, 0, 100], ['B10902076', '闕中豪     ', '芒果', 10, 0, 0, 0, 0, 0], ['B10902078', '李驊祐     ', 'CPass', 60, 100, 100, 100, 50, 0], ['B10902080', '胡家榆     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902088', '周佳葵     ', 'NightCoders', 80, 100, 0, 20, 0, 100], ['B10902090', '鄭家林     ', 'Anonymous', 0, 0, 0, 10, 0, 0], ['B10902100', '嚴詩順     ', 'CPass', 100, 100, 100, 100, 100, 100], ['B10902102', '方炫燁     ', 'Anonymous', 80, 100, 100, 100, 100, 100], ['B10902104', '王彥宗     ', '芒果', 0, 100, 0, 20, 0, 40], ['B10902108', '吳岱凌     ', '芒果', 100, 100, 100, 100, 50, 0], ['B10902110', '呂振嘉     ', 'CPass', 100, 100, 0, 0, 100, 0], ['B10902112', '李梁玉軒    ', 'NightCoders', 100, 100, 100, 100, 100, 100], ['B10902114', '卓引宣     ', '亡靈', 10, 0, 0, 100, 0, 20], ['B10902116', '洪子涵     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902118', '崔俊威     ', '月光', 100, 100, 100, 100, 100, 100], ['B10902120', '張耕綸     ', 'Anonymous', 0, 100, 0, 100, 100, 90], ['B10902122', '許睿尹     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902124', '陳  昕    ', 'NightCoders', 60, 100, 0, 0, 100, 100], ['B10902126', '陳致翰     ', '亡靈', 100, 100, 100, 100, 100, 100], ['B10902130', '黃向誼     ', 'CPass', 0, 10, 0, 0, 0, 0], ['B10902132', '劉\ue06b安     ', 'Anonymous', 100, 10, 100, 100, 0, 90], ['B10902134', '謝侑庭     ', '芒果', 0, 0, 100, 100, 0, 100], ['T10902304', '吳同森     ', 'Anonymous', 0, 0, 0, 0, 0, 0], ['B08902140', '蔡承銜     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09502032', '尤紹宇     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902008', '葉乃瑄     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902020', '黃展翌     ', '計程車', 100, 100, 0, 100, 0, 90], ['B09902041', '陳盛緯     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902056', '陳威宇     ', '芒果', 100, 100, 100, 100, 100, 100], ['B09902059', '楊鎧駿     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B09902063', '董  瑋    ', '派大星', 100, 100, 100, 100, 100, 100], ['B09902084', '千葉\u3000武史   ', 'Anonymous', 40, 0, 0, 0, 0, 0], ['B09902086', '戴安娜保拉阿  ', '芒果', 0, 0, 0, 0, 0, 0], ['B09902116', '林東君     ', '計程車', 100, 100, 100, 100, 100, 100], ['B09902117', '林榮盛     ', '計程車', 100, 100, 100, 100, 100, 100], ['B10902016', '邱子竣     ', '派大星', 100, 100, 100, 100, 100, 100], ['B10902128', '曾宥林     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['B08902096', '艾奕安     ', 'Anonymous', 0, 0, 0, 0, 0, 0], ['B08902150', '謝騏鴻     ', 'Anonymous', 0, 100, 0, 100, 0, 0], ['B06902114', '孫立鴻     ', 'Anonymous', 100, 100, 100, 100, 100, 0], ['B07902092', '林壯謙     ', 'Anonymous', 10, 0, 0, 0, 0, 0], ['R10944021', '王冠人     ', 'Anonymous', 100, 100, 100, 100, 100, 100], ['R10944054', '吳佩玲     ', 'Anonymous', 90, 100, 100, 100, 100, 100], ['R09944011', '羅廣丞     ', 'Anonymous', 0, 100, 0, 100, 100, 100]]
+
+class Data{
+    constructor(args){
+        let def={id:'', name:'', gp:'', gpid:'', score:[], sum:0, avg:0, cls:0, gpsize:1, clssize: 1, rk:1, clsrk:1, gprk:1};
+        Object.assign(def, args), Object.assign(this, def);
+    }
+    sumUp(){
+        for(var i=0; i<this.score.length; ++i)this.sum+=this.score[i];
+        this.avg=this.sum/this.score.length;
+        this.avg=Math.round(this.avg*100)/100;
+    }
+}
+
+class Group{
+    constructor(args){
+        let def={name:'', score:[], sum:0, avg:0, sz:[0, 0], rk:1};
+        Object.assign(def, args), Object.assign(this, def);
+    }
+    sumUp(){
+        for(var i=0; i<this.score.length; ++i)this.sum+=this.score[i];
+        this.avg=this.sum/this.score.length;
+        this.avg=Math.round(this.avg*100)/100;
+    }
+}
+
+class Problem{
+    constructor(args){
+        let def={score:[], sum:0, avg:0, sz:0, vari:0, distr:[]};
+        Object.assign(def, args), Object.assign(this, def);
+    }
+    sumUp(){
+        this.sz=this.score.length;
+        var mx=0;
+        for(var i=0; i<this.sz; ++i)mx=Math.max(mx, this.score[i]/10);
+        for(var i=0; i<=mx; ++i)this.distr.push(0);
+        for(var i=0; i<this.sz; ++i)++this.distr[this.score[i]/10];
+        for(var i=0; i<this.sz; ++i)this.sum+=this.score[i];
+        this.avg=this.sum/this.sz;
+        for(var i=0; i<this.sz; ++i)this.vari+=this.score[i]*this.score[i];
+        this.vari/=this.sz, this.vari-=this.avg*this.avg;
+    }
+    Round(){
+        this.avg=Math.round(this.avg*100)/100;
+    }
+}
+
+function corr(a, b){
+    var ret=0, A=0, B=0;
+    for(var i=0; i<a.sz; ++i)
+        ret+=(a.score[i]-a.avg)*(b.score[i]-b.avg),
+        A+=(a.score[i]-a.avg)*(a.score[i]-a.avg),
+        B+=(b.score[i]-b.avg)*(b.score[i]-b.avg);
+    ret/=Math.sqrt(A*B);
+    return Math.round(ret*100)/100;
+}
+
+var N=[6, 6]; //考試次數
+var M=[80, 77]; //班級人數
+var data=[], gp=[], problem=[], sproblem=[];
+
+for(var i=0; i<pydata.length; ++i){
+    data.push(new Data({id:pydata[i][0], name:pydata[i][1], gp:pydata[i][2], cls:(i<M[0]?0:1), clssize:M[i<M[0]?0:1]}));
+    for(var j=3; j<pydata[i].length; ++j)data[i].score.push(pydata[i][j]);
+    data[i].sumUp();
+    var j=0;
+    for(; j<gp.length; ++j)if(gp[j].name==data[i].gp)break;
+    if(j==gp.length)gp.push(new Group({name:data[i].gp}));
+    ++gp[j].sz[data[i].cls];
+    for(var k=0; k<data[i].score.length; ++k)gp[j].score.push(data[i].score[k]);
+    data[i].gpid=j;
+}
+
+for(var i=0; i<gp.length; ++i){
+    gp[i].sumUp();
+}
+
+for(var i=0; i<data.length; ++i){
+    data[i].gpsize=gp[data[i].gpid].sz[0]+gp[data[i].gpid].sz[1];
+    for(var j=0; j<data.length; ++j){
+        if(data[i].avg<data[j].avg){
+            ++data[i].rk;
+            if(data[i].cls==data[j].cls)++data[i].clsrk;
+            if(data[i].gp==data[j].gp)++data[i].gprk;
+        }
+    }
+}
+
+for(var i=0; i<gp.length; ++i){
+    for(var j=0; j<gp.length; ++j){
+        if(gp[i].avg<gp[j].avg)++gp[i].rk;
+    }
+}
+
+for(var i=0; i<N[0]; ++i){
+    problem.push(new Problem({}));
+    for(var j=0; j<M[0]; ++j)problem[i].score.push(data[j].score[i]);
+}
+sproblem.push(new Problem({}));
+for(var i=0; i<M[0]; ++i){
+    sproblem[0].score.push(0);
+    for(var j=0; j<N[0]; ++j)sproblem[0].score[i]+=problem[j].score[i];
+}
+for(var i=N[0]; i<N[0]+N[1]; ++i){
+    problem.push(new Problem({}));
+    for(var j=M[0]; j<M[0]+M[1]; ++j)problem[i].score.push(data[j].score[i-N[0]]);
+}
+sproblem.push(new Problem({}));
+for(var i=M[0]; i<M[0]+M[1]; ++i){
+    sproblem[1].score.push(0);
+    for(var j=N[0]; j<N[0]+N[1]; ++j)sproblem[1].score[i-M[0]]+=problem[j].score[i-M[0]];
+}
+for(var i=0; i<problem.length; ++i)problem[i].sumUp();
+for(var i=0; i<2; ++i)sproblem[i].sumUp();
+
+if(!!document.getElementById('analysis')){
+    var item=document.getElementById('analysis');
+    item.innerHTML='單班題目與總分相關係數：';
+    for(var i=0; i<N[0]; ++i)item.innerHTML+=String(corr(sproblem[0], problem[i]))+'　';
+    item.innerHTML+='<br>雙班題目與總分相關係數：';
+    for(var i=N[0]; i<N[0]+N[1]; ++i)item.innerHTML+=String(corr(sproblem[1], problem[i]))+'　';
+    item.innerHTML+='<br>　'
+}
+
+if(!!document.getElementById('analysis0')){
+    var item=document.getElementById('analysis0');
+    item.innerHTML='單班總分人數：<br>';
+    for(var i=sproblem[0].distr.length-1; i>=0; --i)if(sproblem[0].distr[i]>0)item.innerHTML+=String(i*10)+'：'+String(sproblem[0].distr[i])+'<br>';
+}
+if(!!document.getElementById('analysis1')){
+    var item=document.getElementById('analysis1');
+    item.innerHTML='雙班總分人數：<br>';
+    for(var i=sproblem[1].distr.length-1; i>=0; --i)if(sproblem[1].distr[i]>0)item.innerHTML+=String(i*10)+'：'+String(sproblem[1].distr[i])+'<br>';
+}
+
+function searchID(){
+    var item=document.getElementById('studentID');
+    var id=0;
+    for(; id<data.length; ++id)if(data[id].id==item.value)break;
+    var item2=document.getElementById('score');
+    if(id==data.length){
+        item2.innerHTML='No data', item.value='';
+        return;
+    }
+    item2.innerHTML="公會："+String(data[id].gp)+"<br>總分："+String(data[id].sum)+"<br>公會排名："+String(data[id].gprk)+'/'+String(data[id].gpsize)+"<br>班級排名："+String(data[id].clsrk)+'/'+String(data[id].clssize)+"<br>全體排名："+String(data[id].rk)+'/'+String(M[0]+M[1])+"<br>";
+    item.value='';
+}
+
+function searchGP(){
+    var item=document.getElementById('gpname');
+    var id=0;
+    for(; id<gp.length; ++id)if(gp[id].name==item.value)break;
+    var item2=document.getElementById('score');
+    if(id==gp.length){
+        item2.innerHTML='No data', item.value='';
+        return;
+    }
+    item2.innerHTML="公會："+String(gp[id].name)+"<br>單班人數："+String(gp[id].sz[0])+"<br>雙班人數："+String(gp[id].sz[1])+"<br>平均每題得分："+String(gp[id].avg)+"<br>排名："+String(gp[id].rk);
+    item.value='';
+}
+
+document.addEventListener('keydown', function(evt){
+    if(evt.keyCode==13){
+        if(!!document.getElementById('studentID'))searchID();
+        else if(!!document.getElementById('gpname'))searchGP();
+    }
+});
